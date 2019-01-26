eb485df42c37af42df68157f822271363dbb4fbc
Guillaume <occuly.guillaume@gmail.com>
2019-01-26 22:44:33 +0100

    Improve ShowtimeShow view

--- src/views/ShowtimeShow.vue
+++ src/views/ShowtimeShow.vue
@@ -6,34 +6,46 @@
         <div class="card">
           <div class="card-body">
             <div class="row">
-              <div class="col-sm-4">
-                <img :src="movie.poster_url" alt="movie.title" />
+              <div class="col-sm-3">
+                <img
+                  :src="movie.attributes.poster_url"
+                  alt="movie.attributes.title"
+                />
               </div>
 
-              <div class="col-sm-4">
+              <div class="col-sm-5">
                 <router-link
                   :to="{ name: 'movie-show', params: { id: movie.id } }"
                 >
-                  <h1>{{ movie.title }}</h1>
+                  <h1 class="text-center">{{ movie.attributes.title }}</h1>
                 </router-link>
                 <hr />
                 <p>
                   <span class="text-muted">During</span>
-                  {{ movie.running_time }} min
+                  {{ movie.attributes.running_time }} min
                 </p>
                 <p>
                   <span class="text-muted">Released on</span>
-                  {{ movie.release_date }}
+                  {{ movie.attributes.release_date }}
                 </p>
-                <p><span class="text-muted">By</span> {{ movie.directors }}</p>
-                <p><span class="text-muted">With</span> {{ movie.casts }}</p>
+                <p><span class="text-muted">By</span> {{ directorNames }}</p>
+                <p><span class="text-muted">With</span> {{ castNames }}</p>
               </div>
 
               <div class="col-sm-4">
-                <h2>{{ cinema.name }}</h2>
+                <h2 class="text-center">{{ cinema.attributes.name }}</h2>
                 <hr />
                 <p class="text-muted">Address</p>
-                <p>{{ cinema.address }}</p>
+                <p v-html="computedAddress"></p>
+
+                <div class="text-center">
+                  <a
+                    :href="googleMapsUrl"
+                    class="btn btn-outline-success"
+                    target="_blank"
+                    >Get directions</a
+                  >
+                </div>
               </div>
             </div>
 
@@ -42,27 +54,31 @@
               <div class="row">
                 <div class="col-sm">
                   <p>Date</p>
-                  <p class="text-danger">{{ showtime.start_date }}</p>
+                  <p class="text-danger">
+                    {{ showtime.attributes.start_date }}
+                  </p>
                 </div>
                 <div class="col-sm">
                   <p>Begins</p>
-                  <p class="text-danger">{{ showtime.start_time }}</p>
+                  <p class="text-danger">
+                    {{ showtime.attributes.start_time }}
+                  </p>
                 </div>
                 <div class="col-sm">
                   <p>Ends</p>
-                  <p class="text-danger">{{ showtime.end_time }}</p>
+                  <p class="text-danger">{{ showtime.attributes.end_time }}</p>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-sm">
                   <p>Dimension</p>
-                  <p class="text-danger">{{ showtime.dimension }}</p>
+                  <p class="text-danger">{{ showtime.attributes.dimension }}</p>
                 </div>
                 <div class="col-sm align-self-center">
-                  <template v-if="showtime.booking_link">
+                  <template v-if="showtime.attributes.booking_link">
                     <a
-                      :href="showtime.booking_link"
+                      :href="showtime.attributes.booking_link"
                       class="btn btn-outline-success"
                       target="_blank"
                       >Book Online</a
@@ -71,7 +87,7 @@
                 </div>
                 <div class="col-sm">
                   <p>Version</p>
-                  <p class="text-danger">{{ showtime.version }}</p>
+                  <p class="text-danger">{{ showtime.attributes.version }}</p>
                 </div>
               </div>
             </div>
@@ -102,10 +118,39 @@ export default {
   },
   created() {
     MOTService.getShowtime(this.id).then(response => {
-      this.showtime = response.data["showtime"];
-      this.movie = this.showtime.movie;
-      this.cinema = this.showtime.cinema;
+      this.showtime = response.data["data"];
+      this.movie = this.showtime.attributes.movie.data;
+      this.cinema = this.showtime.attributes.cinema.data;
     });
+  },
+  computed: {
+    castNames: function() {
+      var casts = this.movie.attributes.casts.data.map(
+        cast => cast.attributes.name
+      );
+      return casts.join(", ");
+    },
+    directorNames: function() {
+      var directors = this.movie.attributes.directors.data.map(
+        director => director.attributes.name
+      );
+      return directors.join(", ");
+    },
+    computedAddress: function() {
+      return [
+        this.cinema.attributes.street,
+        this.cinema.attributes.post_code,
+        this.cinema.attributes.city
+      ].join("<br>");
+    },
+    googleMapsUrl: function() {
+      return (
+        "https://www.google.com/maps/dir//" +
+        this.cinema.attributes.latitude +
+        "," +
+        this.cinema.attributes.longitude
+      );
+    }
   }
 };
 </script>
