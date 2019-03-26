a73a66331c4a7a50535690ba3327a25a5052b830
Guillaume <occuly.guillaume@gmail.com>
2019-03-26 19:23:21 +0100

    Add AppSearchMovies as global component

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,30 +2,7 @@
   <div>
     <MovieHeaderLinkList />
 
-    <div class="row mt-2" v-if="movies.length > 0">
-      <form class="form-inline">
-        <div class="form-group mx-sm-3 mb-2">
-          <label for="inputPassword2" class="sr-only">Password</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Search movies"
-            v-model="query"
-            @input="queryEntered"
-          />
-        </div>
-        <div class="form-group mb-2">
-          <label for="staticEmail2" class="sr-only">Email</label>
-          <input
-            type="text"
-            readonly
-            class="form-control-plaintext"
-            id="staticEmail2"
-            :value="'Movies: ' + movies.length"
-          />
-        </div>
-      </form>
-    </div>
+    <AppSearchMovies @query-entered="queryEntered" :movies="movies" />
 
     <AppPagination
       :meta="meta"
@@ -64,17 +41,11 @@ export default {
       type: Array
     }
   },
-  data() {
-    return {
-      query: ""
-    };
-  },
   methods: {
     paginationClicked: function(payload) {
       this.$emit("pagination-clicked", payload);
     },
-    queryEntered: function() {
-      const payload = { query: this.query };
+    queryEntered: function(payload) {
       this.$emit("query-entered", payload);
     }
   }
