390105b072c2259b87fc51150b6dd62778f70f7b
Guillaume <occuly.guillaume@gmail.com>
2019-07-13 19:51:56 +0200

    Remove "search" button on navbar
    We only need to press enter to search movies

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-white">
+  <nav class="navbar navbar-expand bg-white">
     <div class="container">
-      <router-link to="/" class="navbar-brand">Movie On Time</router-link>
+      <router-link to="/" class="navbar-brand main-title"
+        >Movie On Time</router-link
+      >
 
       <button
         class="navbar-toggler"
@@ -17,22 +19,15 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <form class="form-inline my-2 my-lg-0">
+          <form class="form-inline" @submit.prevent="submit">
             <input
-              class="form-control mr-sm-2"
+              class="form-control mr-sm-1"
               type="search"
               v-model="query"
               autofocus
-              placeholder="Enter a movie title"
+              placeholder="Search"
               aria-label="Search"
             />
-            <button
-              class="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-              @click.stop.prevent="submit()"
-            >
-              Search
-            </button>
           </form>
         </ul>
 
@@ -102,8 +97,11 @@ export default {
 };
 </script>
 <style scoped>
+.main-title {
+  color: #484848;
+}
 .container {
-  max-width: 1300px;
+  max-width: 1275px;
 }
 a:hover {
   color: #e53935;
