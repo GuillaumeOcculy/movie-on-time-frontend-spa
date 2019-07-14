1aa750031b43cc6172ad059e05892b3953b439a3
Guillaume <occuly.guillaume@gmail.com>
2019-07-14 23:53:23 +0200

    Update MovieHeaderLinkList.vue

--- src/components/MovieHeaderLinkList.vue
+++ src/components/MovieHeaderLinkList.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="mt-3">
     <router-link to="/" class="navbar-brand">Now Showing</router-link>
-    <router-link :to="{ name: 'premiere' }" class="navbar-brand"
-      >Premiere</router-link
-    >
+
     <router-link :to="{ name: 'upcoming' }" class="navbar-brand"
       >Upcoming</router-link
+    >
+    <router-link :to="{ name: 'premiere' }" class="navbar-brand"
+      >Premiere</router-link
     >
     <router-link :to="{ name: 'reprojection' }" class="navbar-brand"
       >Reprojection</router-link
