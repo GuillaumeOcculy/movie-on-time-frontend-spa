7b08a94635616636766034ceffb8d95cde071900
Guillaume <occuly.guillaume@gmail.com>
2019-07-26 20:46:35 +0200

    Add .env & .env.production files
    Add MOT base url to avoid changing everytime

--- src/services/MOTService.js
+++ src/services/MOTService.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import NProgress from "nprogress";
 import queryString from "query-string";
 
-const baseUrl = "http://localhost:3000";
-// const baseUrl = "https://api.movieontime.com";
+const baseUrl = process.env.VUE_APP_MOT_API_URL;
 
 const apiClient = axios.create({
   baseURL: baseUrl,
