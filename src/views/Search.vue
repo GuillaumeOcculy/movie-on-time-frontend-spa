7dee23161e773a5a23ac2143e13b1b7c9dee032c
Guillaume <occuly.guillaume@gmail.com>
2019-07-30 23:39:24 +0200

    Show explicit message on what we  are doing on Search vue

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <h1>Search</h1>
     <div class="row">
-      <template v-if="movieCount > 0">
-        <MovieListItem v-for="movie in movies" :key="movie.id" :movie="movie" />
-      </template>
-      <template v-else>
-        <div class="mt-3">
-          <h2 class="text-center">
-            We did not found movies with this query: {{ movieQuery }}
-          </h2>
-        </div>
-      </template>
+      <MovieListItem v-for="movie in movies" :key="movie.id" :movie="movie" />
+    </div>
+
+    <div class="mt-3 text-center">
+      <h2 v-if="movieCount == 0 && doneFetching">
+        We did not found movies with this query: {{ movieQuery }}
+      </h2>
+      <h2 v-if="!doneFetching">Loading...</h2>
     </div>
   </div>
 </template>
@@ -26,7 +23,8 @@ export default {
   },
   data() {
     return {
-      movies: []
+      movies: [],
+      doneFetching: false
     };
   },
   methods: {
@@ -34,6 +32,7 @@ export default {
       MOTService.searchMovies(this.$route.query)
         .then(response => {
           this.movies = response.data["data"];
+          this.doneFetching = true;
         })
         .catch(error => {
           console.log("There was an error:", error.response);
