0bba5f74425fc988e2f5c8bf9b35e8d9867d5b1f
Guillaume <occuly.guillaume@gmail.com>
2019-08-31 14:29:37 +0200

    Fix MovieHours duration

--- src/components/MovieHours.vue
+++ src/components/MovieHours.vue
@@ -14,7 +14,10 @@ export default {
   computed: {
     hours: function() {
       const hour = Math.floor(this.running_time / 60);
-      const min = this.running_time % 60;
+      let min = this.running_time % 60;
+      if (min.toString().length == 1) {
+        min = "0" + min;
+      }
 
       return hour + "h" + min;
     }
