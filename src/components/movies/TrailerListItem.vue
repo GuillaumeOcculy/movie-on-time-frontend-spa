a28abb4ee82365cbedf4d2fc7910ce09054239a1
Guillaume <occuly.guillaume@gmail.com>
2019-08-31 13:44:40 +0200

    Allow fullscreen on embed (trailer)

--- src/components/movies/TrailerListItem.vue
+++ src/components/movies/TrailerListItem.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="col-sm-12 col-lg-6 mb-3">
     <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" :src="trailerUrl"></iframe>
+      <iframe
+        class="embed-responsive-item"
+        :src="trailerUrl"
+        allowfullscreen
+      ></iframe>
     </div>
   </div>
 </template>
